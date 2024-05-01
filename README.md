5d8c717f7184abbe9a4e010b571c58795130c3fd
Choi <86215536+hye-jj@users.noreply.github.com>
2024-05-01 13:44:59 +0900

    naver crawling 종료

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# movie_crawling
- 
+# movie_crawling 
+- 영화목록 정보 출처 : KOBIS 영화정보 (https://www.kobis.or.kr/kobis/business/mast/mvie/searchMovieList.do)
+
+
+## 개요
+해당 프로젝트는 네이버 영화리뷰 정보제공이 종료되어,            
+2023년 이후의 영화에 대해 영화리뷰(관람평)을 수집하기 위해 만들어졌습니다.
+
+## 이후과제
+- [ ] 영화리뷰 데이터 확인, 형태소 분석, 전처리
+- [ ] LDA 토픽모델링
+- [ ] LSTM, GRU 모델 감성분석
+
+
---
+++ naver_crawling_수정.py
@@ -0,0 +1,98 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import os
+
+# 영화목록 읽기
+import pandas as pd
+
+cols = list(pd.read_csv('영화데이터_항목명.txt'))
+col_li = []
+for i in cols:
+    col_li.append(i.strip())
+
+movie_li = pd.read_csv('영화데이터(KOBIS_개봉일람_20240408조회).txt', sep='|')
+movie_li = movie_li.T.reset_index(drop=True).T
+movie_li.columns = col_li
+movie_li.sort_values(by=['전국 관객수'])
+movie_li = movie_li[movie_li['전국 관객수'] > 1000]
+# print(movie_li.shape)
+# movie_li.head(1)
+
+os.makedirs('./data', exist_ok=True)
+
+def movie_crawl(movie_name):
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.get('https://search.naver.com/')
+        elem = driver.find_element_by_id('query')
+        elem.send_keys('영화 '+ movie_name + " 관람평")
+        elem.send_keys(Keys.RETURN)
+
+        # 최신순 클릭
+        latest = driver.find_element(By.XPATH, f'//*[@id="main_pack"]/div[3]/div[2]/div/div/div[4]/div[2]/div[1]/div/ul/li[2]/a')
+        latest.click()
+        
+        actions = driver.find_element(by=By.CLASS_NAME, value='lego_review_list._scroller')
+        # actions = driver.find_element(by=By.CSS_SELECTOR, value='#main_pack > div.sc_new.cs_common_module.case_empasis.color_4._au_movie_content_wrap > div.cm_content_wrap > div > div > div:nth-child(4) > div.lego_review_list._scroller')
+        time.sleep(1)
+        location = actions.location_once_scrolled_into_view
+        time.sleep(1)
+
+        try:
+
+                for i in range(1,1000):
+                    data1 = driver.find_element(By.XPATH, f'//*[@id="main_pack"]/div[3]/div[2]/div/div/div[4]/div[4]/ul/li[{i}]')
+                    time.sleep(1)
+                    location1 = data1.location_once_scrolled_into_view
+                    time.sleep(1)
+                    
+                    
+                    
+                    try:
+                        btn = driver.find_element(By.XPATH, f'//*[@id="main_pack"]/div[3]/div[2]/div/div/div[4]/div[4]/ul/li[{i}]/div[2]/div/button')
+                        btn.click()
+                        time.sleep(1)
+                        
+                    except Exception as e:
+                        print('예외가 발생했습니다(클릭실패). ')
+                        pass
+
+        except Exception as e:
+            print('예외가 발생했습니다. ') #, e
+
+
+        finally:
+            raw_data = actions.text
+            raw_li = raw_data.split('별점(10점 만점 중)') 
+            df = pd.DataFrame(index=range(len(raw_li)), columns=['리뷰'], data=raw_li) 
+            df['영화명'] = movie_name
+
+            filename = f"./data/{movie_name.split(':')[0]}_latest2_review_1000000.pkl"
+            df.to_pickle(filename)
+
+            time.sleep(3)
+            driver.close()
+            print(filename + "  저장!!!")
+
+    
+    
+
+
+if __name__ == "__main__":
+    # start_time을 체크
+    start_time = time.time()
+
+    for movie_name in movie_li['영화명'][:2]:
+        try :
+            movie_crawl(movie_name)
+        except:
+            print(movie_name)
+
+    print("---{}s seconds---".format(time.time()-start_time))
+            
+        
